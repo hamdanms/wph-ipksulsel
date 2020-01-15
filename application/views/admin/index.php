4534713a3f034f380b697bd926559b751d7a07e0
Hamdanms <Hamdanjhee21@gmail.com>
2020-01-15 16:21:34 +0800

    menambahkan fitur laba bulanan

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -11,6 +11,10 @@
     
     <!-- Content Row -->
     <div class="row">
+        <?php
+            $kasbulanan = $kasm['0']['saldo'];
+            $laba = $kasbulanan - $kask ;
+        ?>
         <!-- Earnings (Monthly) Card Example -->
         <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-primary shadow h-100 py-2">
@@ -61,9 +65,25 @@
             </div>
             </div>
         </div>
+        <!-- Earnings (Monthly) Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-info shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                    <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Laba bulan ini</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800"><?= "Rp " . number_format($laba,0 ,',','.') ;?></div>
+                </div>
+                <div class="col-auto">
+                    <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
 
         <!-- Pending Requests Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
+        <!-- <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-warning shadow h-100 py-2">
             <div class="card-body">
                 <div class="row no-gutters align-items-center">
@@ -77,8 +97,8 @@
                 </div>
             </div>
             </div>
-        </div>
-    </div>
+        </div>-->
+    </div> 
 
         <!-- Content Row -->
  
