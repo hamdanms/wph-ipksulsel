df03ccb979fe1a467016ea915a9a692042106e5e
Hamdanms <Hamdanjhee21@gmail.com>
2020-01-16 20:10:00 +0800

    konfigurasi base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/wph-ipksulsel';
+$config['base_url'] = 'http://ipksulsel.com';
 
 /*
 |--------------------------------------------------------------------------
